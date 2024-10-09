18a45d87d9658650a772988e9a1434e69c91b751
btalha23 <161279133+btalha23@users.noreply.github.com>
2024-10-09 09:28:31 +0200

    ¤ setup/setup_ec2.sh - resolving errors while running the make file

--- setup/setup_ec2.sh
+++ setup/setup_ec2.sh
@@ -47,7 +47,7 @@ echo "aws cli version..."
 aws --version
 
 echo "Installing MySQL..."
-cd ~/software && wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
+cd ~/software && wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
 
 sudo apt update
