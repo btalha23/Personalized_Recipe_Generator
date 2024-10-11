0dfe8d2c96087dddca8360b08c0c34be5e19f423
btalha23 <161279133+btalha23@users.noreply.github.com>
2024-10-11 07:43:41 +0200

    ¤ setup/setup_ec2.sh - added the part to install and initialize MySQL

--- setup/setup_ec2.sh
+++ setup/setup_ec2.sh
@@ -46,14 +46,29 @@ cd ~/software && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 echo "aws cli version..."
 aws --version
 
-echo "Installing MySQL..."
-sudo apt install mysql-server
+echo "Initializing the password variable"
+read -sp "Press ENTER as your password for MySQL: " mysqlpswd
 
-sudo systemctl start mysql 
+echo "Installing MySQL..."
+sudo apt install mysql-server -y
+
+echo "mysql version..."
+mysql --version
+
+echo "enable MySQL to automatically start at boot time...."
 sudo systemctl enable mysql 
 
+echo "start the MySQL database server...."
+sudo systemctl start mysql 
 
+echo "view the MySQL database server status...."
+sudo systemctl status mysql
 
+echo "Log in to the MySQL database server as root...."
+sudo mysql -u root -pmysqlpswd
+
+echo "Change root user password..."
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 
 echo "Activate conda environment..."
 conda activate
