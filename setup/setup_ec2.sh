4947539f812779b3dcb41a4017a805861908fbdc
btalha23 <161279133+btalha23@users.noreply.github.com>
2024-10-20 00:41:53 +0200

    ¤ setup/setup_ec2.sh - bugfix; correction in the command for downloading a specific version of streamlit

--- setup/setup_ec2.sh
+++ setup/setup_ec2.sh
@@ -80,7 +80,7 @@ echo "Installing necessary packages..."
 
 pip install pandas numpy matplotlib seaborn wordcloud scikit-learn keras tabulate
 pip install langchain langchain_community langchain_experimental langchain_openai langsmith openai
-pip install streamlit=1.38.0 
+pip install streamlit==1.38.0 
 pip install streamlit_feedback
 pip install mysql-connector-python sqlalchemy
 pip install python-dotenv
